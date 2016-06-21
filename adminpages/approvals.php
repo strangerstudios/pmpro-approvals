defd5496b02799e6c94c663499d4abc2f383cb4e
Andrew Lima <andrew@arctek.co.za>
2016-06-21 17:24:45 +0200

    First Commit
    Setup for PMPro Approvals Add-on

--- adminpages/approvals.php
+++ adminpages/approvals.php
@@ -3,11 +3,11 @@
 	if ( ! function_exists( "current_user_can" ) || ( ! current_user_can( "manage_options" ) && ! current_user_can( "pmpro_approvals" ) ) ) {
 		die( __( "You do not have permissions to perform this action.", "pmproapp" ) );
 	}
-	
+
 	require_once( PMPRO_DIR . "/adminpages/admin_header.php" );
 ?>
 <h2>Approvals</h2>
-
+<!-- This would be the list of outstanding members? -->
 <p>Content will go here.</p>
 <?php
 	require_once( PMPRO_DIR . "/adminpages/admin_footer.php" );
