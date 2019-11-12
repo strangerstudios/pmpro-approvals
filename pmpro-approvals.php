c87a37cc908558c6ca71d050c865c940bb96bf3f
Jason Coleman <jason@strangerstudios.com>
2019-11-12 10:39:08 -0500

    fixing a notice

--- pmpro-approvals.php
+++ pmpro-approvals.php
@@ -477,7 +477,7 @@ class PMPro_Approvals {
 			// Ignore if on the edit user screen. This will allow admins/users to update custom fields.
 			$current_screen = get_current_screen();
 			
-			if ( $current_screen->base == 'user-edit' || $current_screen->base == 'profile' ) {
+			if ( !empty( $current_screen ) && ( $current_screen->base == 'user-edit' || $current_screen->base == 'profile' ) ) {
 				return $haslevel;
 			}
 
