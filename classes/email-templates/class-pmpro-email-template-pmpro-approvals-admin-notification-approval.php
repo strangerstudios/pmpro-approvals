5c7b515b09c1fb3177e15869bd8086b3a3cfea05
David Wanjuki <123358258+dwanjuki@users.noreply.github.com>
2025-05-19 18:08:38 +0300

    Update class-pmpro-email-template-pmpro-approvals-admin-notification-approval.php

--- classes/email-templates/class-pmpro-email-template-pmpro-approvals-admin-notification-approval.php
+++ classes/email-templates/class-pmpro-email-template-pmpro-approvals-admin-notification-approval.php
@@ -173,7 +173,7 @@ class PMPro_Email_Template_PMProApprovals_Admin_Notification_Approval extends PM
  * @return array The modified email templates array.
  */
 function pmpro_email_template_pmpro_approvals_admin_notification_approval( $email_templates ) {
-	$email_templates['admin_notification_approval'] = 'PMPro_Email_Template_PMProApprovals_Admin_Notification_Approval';
+	$email_templates['admin_notification_approval_admin'] = 'PMPro_Email_Template_PMProApprovals_Admin_Notification_Approval';
 	return $email_templates;
 }
 add_filter( 'pmpro_email_templates', 'pmpro_email_template_pmpro_approvals_admin_notification_approval' );
